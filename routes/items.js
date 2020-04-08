2bebfa8cf8f07a3143c3386cd4dac1a804cd23a8
Jovan <jovandjukic2@gmail.com>
2020-04-08 17:01:04 +0200

    Items route validation

--- routes/items.js
+++ routes/items.js
@@ -3,27 +3,45 @@ const express = require("express");
 const router = express.Router();
 const { Op } = require("sequelize");
 const debug = require("debug")("agromarket");
+const Joi = require("joi");
+const validator = require("express-joi-validation").createValidator({});
 
-router.get("/", async (req, res) => {
+const itemSchema = Joi.object({
+  title: Joi.string(),
+  radius: Joi.number().precision(3),
+  diameter: Joi.number().precision(3),
+  b: Joi.number().precision(3),
+  page: Joi.number(),
+  size: Joi.number(),
+});
+
+router.get("/", validator.query(itemSchema), async (req, res) => {
+  const { radius, diameter, b } = req.query;
   const title = req.query.title || "";
-  const page = +req.query.page || 1;
-  const size = +req.query.size || 10;
-  const diameter = req.query.diameter;
-  const radius = req.query.radius;
-  const b = req.query.b;
+  const page = req.query.page || 1;
+  const size = req.query.size || 10;
 
   const findOptions = {
     where: {
       title: {
         [Op.substring]: title,
       },
-      radius,
-      diameter,
-      b,
     },
     offset: (page - 1) * size,
     limit: size,
   };
+
+  if (radius) {
+    findOptions.radius = radius;
+  }
+
+  if (diameter) {
+    findOptions.diameter = diameter;
+  }
+
+  if (b) {
+    findOptions.b = b;
+  }
 
   const items = await models.item.findAll(findOptions);
 
